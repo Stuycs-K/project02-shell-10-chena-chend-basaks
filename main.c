1d9d6624d00778cfc413eee366c73356f7ebc333
David Chen <dchen52@stuy.edu>
2024-12-02 14:35:31 -0600

    started parsing

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 int main () {
-  
+
+
+
+	char* commandBuffer = malloc(256);
+	char* currentSegment = malloc(256);
+	fgets(commandBuffer, 255, stdin);
+	while(currentSegment = strsep(&commandBuffer, " ")){
+		printf("%s \n", currentSegment);
+
+	}
+
 }
