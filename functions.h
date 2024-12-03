614286d16c34fc8adc45279daf6837f674397aa1
Souvik Basak <souvik.b416@gmail.com>
2024-12-03 15:09:06 -0500

    executeCommand

--- functions.h
+++ functions.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+void executeCommand(char *command);
 #endif
--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "functions.h"
+
 int main () {
 	char* inputBuffer = (char*) malloc(256);
 	fgets(inputBuffer, 255, stdin);
     char* commandBuffer;
 	while((commandBuffer = strsep(&inputBuffer, ";"))){
 		//function to execute a command
-		char* tokenBuffer;
-		while(tokenBuffer  = strsep(&commandBuffer, " ")){
-			printf("%s \n", tokenBuffer); 
-
-		}
-		printf("new command \n");
-
+        executeCommand(commandBuffer);
 	}
 }
