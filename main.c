b57395e365dab8b3c93fab23765d3155134770d5
DavidC251 <dchen52@stuy.edu>
2024-12-02 22:19:45 -0500

    more parsing

--- main.c
+++ main.c
@@ -4,8 +4,15 @@
 int main () {
 	char* inputBuffer = (char*) malloc(256);
 	fgets(inputBuffer, 255, stdin);
-    char* commandBuffer = inputBuffer;
+    char* commandBuffer = (char*) malloc(256);
 	while((commandBuffer = strsep(&inputBuffer, ";"))){
 		//function to execute a command
+		char* tokenBuffer = (char*) malloc(256);
+		while(tokenBuffer  = strsep(&commandBuffer, " ")){
+			printf("%s \n", tokenBuffer); 
+
+		}
+		printf("new command \n");
+
 	}
 }
