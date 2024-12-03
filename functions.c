6e4968ed3070db37d4b28198b0c9689e7b51227b
David Chen <dchen52@stuy.edu>
2024-12-03 15:08:16 -0500

    temp

--- functions.c
+++ functions.c
@@ -1,3 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "functions.h"
+
+
+char* GreaterThan(char* a, char* b){
+
+}
+
+
+
+char* LessThan(char a*, char b*){
+
+
+}
+
+
+
+
+void Pipe(char* a, char* b){
+
+
+}
--- functions.h
+++ functions.h
@@ -1,4 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+void executeCommand(char* command);
+char* GreaterThan(char* a, char* b);
+char* LessThan(char a*, char b*);
+void Pipe(char* a, char* b)
 #endif
