3be1f507af5d3e38c4dd58ed97981bc4be901a7c
Souvik Basak <souvik.b416@gmail.com>
2024-12-03 15:13:06 -0500

    function header file

--- functions.c
+++ functions.c
@@ -1,3 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "functions.h"
+
+void executeCommand(char *command);
+char *redirectStdout(char *command, char *output);
+char *redirectStdin(char *command, char *input);
+void pipe(char *a, char *b);
--- functions.h
+++ functions.h
@@ -1,4 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+void executeCommand(char *command);
+char *redirectStdout(char *command, char *output);
+char *redirectStdin(char *command, char *input);
+void pipe(char *a, char *b);
 #endif
