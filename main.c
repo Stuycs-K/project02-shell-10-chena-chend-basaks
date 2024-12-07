17b4b4a946d3e3949a259db8f9da4ebbed939717
DavidC251 <dchen52@stuy.edu>
2024-12-06 23:02:46 -0500

    finihsed

--- main.c
+++ main.c
@@ -4,16 +4,24 @@
 #include "functions.h"
 #include <signal.h>
 #include <string.h>
+#include <unistd.h>
 int main () {
 	int running = 0;
+	signal(SIGINT, sigs);
+	
 	while(running == 0){
-		signal(SIGINT, sigs);
+		
 		char* inputBuffer = (char*) malloc(256);
+		char* pathBuff = (char*) malloc(256);
+		getcwd(pathBuff, 255);
+		printf("%s$ ", pathBuff);
 		fgets(inputBuffer, 255, stdin);
 	  char* commandBuffer;
 		if(strcmp(inputBuffer, "exit")  == 0){
 			exit(0);
 		}
+	
+		
 		running = quit(0);
 		while((commandBuffer = strsep(&inputBuffer, ";"))){
 			//function to execute a command
@@ -21,14 +29,21 @@ int main () {
 					parseCommand(commandBuffer);
 
 		}
+		
+		
+		fflush(stdout);
+		
 
 	}
+	fflush(stdout);
 	exit(0);
 
 }
 
 static void sigs(int signo) {
+	
     if ( signo == SIGINT ){
+      
       quit(1);
     }
 }
